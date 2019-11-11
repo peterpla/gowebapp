43977b7d42b36ac076d1284b751ef764284e29fb
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-11-11 12:54:22 -0700

    feat: implement basic request logging
    Implement basic request logging, disabled when running
    on GAE (which already logs requests)

--- gdeploy.sh
+++ gdeploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-gcloud meta list-files-for-upload
+# gcloud meta list-files-for-upload
 # verbosity: debug, info, warning, error, critical, none
 gcloud beta app deploy --verbosity=warning app.yaml
 #    --service-account gowebapp-gcs-reader@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com 
