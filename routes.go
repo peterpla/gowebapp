2c76d65bef698bdb77e983dfdb291d69dd56789f
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-11-11 09:58:50 -0700

    chore: dump routes (commented out)
    Dump routes after they're all added, commented out

--- routes.go
+++ routes.go
@@ -8,4 +8,8 @@ func (srv *server) routes() {
 	http.HandleFunc("/", srv.handleHomeOld) // not equivalent to s.router.HandleFunc ???
 	http.HandleFunc("/home", srv.handleHomeOld)
 	http.HandleFunc("/about", srv.handleAbout)
+
+	// show all routes
+	// v := reflect.ValueOf(http.DefaultServeMux).Elem()
+	// log.Printf("routes: %+v\n", v.FieldByName("m"))
 }
