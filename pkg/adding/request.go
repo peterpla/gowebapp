ab532c02a7fb9ccfbc2a63cfc9fbc7d7cd6a7bd6
Peter Plamondon <394133+peterpla@users.noreply.github.com>
2019-12-14 09:41:24 -0700

    feat: implement response body per interface.MD
    Github issue #33. Capture AcceptedAt time as UTC in adding.AddRequest.
    Created adding.ReqResponse struct to hold HTTP response fields.

--- pkg/adding/request.go
+++ pkg/adding/request.go
@@ -1,12 +1,24 @@
 package adding
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 // Request defines properties of an incoming transcription request
 // to be added
 type Request struct {
 	RequestID    uuid.UUID `json:"request_id"`
 	CustomerID   string    `json:"customer_id"`
-	MediaFileURL string    `json:"media_url"`
+	MediaFileURI string    `json:"media_uri"`
 	CustomConfig bool      `json:"custom_config"`
+	AcceptedAt   time.Time `json:"accepted_at"`
+}
+
+type ReqResponse struct {
+	RequestID    uuid.UUID `json:"request_id"`
+	CustomerID   string    `json:"customer_id"`
+	MediaFileURI string    `json:"media_uri"`
+	AcceptedAt   string    `json:"accepted_at"`
 }
